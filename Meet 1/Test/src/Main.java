1c8e4f37c8663d9e2e5d5f52c240e80b0773cf81
youhane <jason.sebastian154@gmail.com>
2021-11-08 07:37:27 +0700

    Meet 1

---
+++ Meet 1/Test/src/Main.java
@@ -0,0 +1,117 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+//	int x = 20;	// global
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// PRIMITIVE DATA TYPE
+//		int a = 10;	// lokal
+//		char b = 'A';
+//		double c = 7.8;
+//		String d = "Hello";
+//
+//		// ABSTRACT DATA TYPE
+//		Stack<Integer> intStack = new Stack<Integer>();
+//		intStack.push(7);
+//		intStack.push(5);
+//		
+//		// OUTPUT
+//		System.out.printf("Hello World\n");
+//		System.out.println("Hello World");
+//		System.out.print("Hello World");
+		
+		// INPUT
+//		int umur;
+//		String nama;
+//		Scanner input = new Scanner(System.in);
+//		System.out.print("Hello, what's your name: ");
+//		nama = input.nextLine();
+//		System.out.print("How old are you: ");
+//		umur = input.nextInt();
+//		input.nextLine();
+//		System.out.println("Hello " + nama + " your age is " + umur);
+//		System.out.printf("Hello %s your age is %d\n", nama, umur);
+		
+		//OPERATOR
+		//1. ARITHMETIC
+//		System.out.println(1 + 1);
+//		System.out.println(5 - 2);
+//		System.out.println(7 * 4);
+//		System.out.println(18 / 3);
+//		System.out.println(5 % 2);
+		
+		//2. RELATIONAL
+//		System.out.println(5 <= 2);
+//		System.out.println(7 > 3);
+//		System.out.println(8 == 8);
+//		System.out.println(9 != 2);
+		
+		//3. LOGICAL
+//		System.out.println(5 > 2 && 7 > 9);		// AND
+//		System.out.println(5 > 2 || 8 > 10);	// OR
+		
+		//4. UNARY OPERATOR
+//		int f = 50;
+//		System.out.println(++f);	//PREFIX
+//		System.out.println(f++);	//POSTFIX
+//		System.out.println(--f);
+		
+		//5. BITWISE DAN SHIFT OPERATOR
+//		1001 (bilangan biner)
+//		8 + 0 + 0 + 1 = 9 (bilangan bulat)
+//		System.out.println(8 << 2);
+//		8	:	0000 1000
+//		32	:	0010 0000
+		
+		//6. ASSIGNMENT OPERATOR
+//		int a = 7, b = 9;
+//		a += b
+//		a = a + b
+//		System.out.println(a += b);
+//		System.out.println(a);
+		
+		//SELECTION
+//		int umur = 14;
+//		if(umur > 17) {
+//			System.out.println("Dewasa");
+//		}else if(umur > 12) {
+//			System.out.println("Remaja");
+//		}else {
+//			System.out.println("Anak-anak");
+//		}
+		
+//		switch(umur) {
+//			case 14:
+//				System.out.println("Empat belas");
+//				break;
+//			case 12:
+//				System.out.println("Dua belas");
+//				break;
+//			default:
+//				System.out.println("Lain-lain");
+//		}
+//		
+		//Ternary operator	? :
+//		System.out.println(umur > 10 ? "Remaja" : "Belum");
+		
+		//REPETITION
+//		for(int i=1; i<=10; i++) {
+//			System.out.println(i);
+//		}
+		
+		// int x = 10;
+//		while(x >= 1) {
+//			System.out.println(x);
+//			x--;
+//		}
+		
+		// do {
+		// 	System.out.println(x);
+		// 	x--;
+		// }while(x >= 1);
+	}
+
+}
