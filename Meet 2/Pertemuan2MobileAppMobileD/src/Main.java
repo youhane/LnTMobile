14da1622aa3edbcf92aa02dc823e83c3dd5236dd
youhane <jason.sebastian154@gmail.com>
2021-11-13 11:26:01 +0700

    meet 2

---
+++ Meet 2/Pertemuan2MobileAppMobileD/src/Main.java
@@ -0,0 +1,120 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// STRING MANIPULATION
+		// LENGTH()
+//		String name = "Max Verstappen";
+//		System.out.println(name.length());
+//		String member = "Arrick Russell Adinoto";
+//		System.out.println(member.length());
+		
+		// STARTSWITH()
+//		String name = "Max Verstappen";
+//		System.out.println(name.startsWith("Max Ver"));
+//		String member = "Arrick Russell Adinoto";
+//		System.out.println(member.startsWith("Ar"));
+		
+		// ENDSWITH()
+//		String name = "Max Verstappen";
+//		System.out.println(name.endsWith("n"));
+//		String member = "Arrick Russell";
+//		System.out.println(member.endsWith("russell"));
+		
+		// EQUALS()
+//		String name = "Max Verstappen";
+//		System.out.println(name.equals("Max Verstappen"));
+		
+		// EQUALSIGNORECASE()
+//		String name = "Max Verstappen";
+//		System.out.println(name.equalsIgnoreCase("MAX VERSTAPPEN"));
+		
+		// TOLOWERCASE()
+//		String name = "MAX VERSTAPPEN";
+//		System.out.println(name.toLowerCase());
+		
+		// TOUPPERCASE()
+//		String name = "max verstappen";
+//		System.out.println(name.toUpperCase());
+		
+		// CONTAINS()
+//		String name = "Max Verstappen";
+//		System.out.println(name.contains("Ver"));
+//		String member = "Arrick Russell";
+//		System.out.println(member.contains("ick"));
+		
+		// CONCAT()
+//		String name = "Max Verstappen";
+//		System.out.println(name.concat(" Emilian"));
+		
+		// SUBSTRING()
+//		String name = "Max Verstappen";
+//		System.out.println(name.substring(4, 10));
+//		String member = "Jason Sebastian";
+//		System.out.println(member.substring(6, 9));
+		
+		//MATH FUNCTION
+		//SQRT
+//		int number = 81;
+//		int result = (int)(Math.sqrt(number));
+//		System.out.println(result);
+		
+		//POW
+//		int x = 5;
+//		int y = 3;
+//		int result = (int)(Math.pow(y, x));
+//		System.out.println(result);
+		
+		//CEIL
+//		double dec = 7.33;
+//		int result = (int)(Math.ceil(dec));
+//		System.out.println(result);
+		
+		//FLOOR
+//		double dec = 8.98;
+//		int result = (int)(Math.floor(dec));
+//		System.out.println(result);
+		
+		//ROUND
+//		double dec = 5.43;
+//		double dec2 = 5.61;
+//		double dec3 = 6.5;
+//		int result = (int)(Math.round(dec));
+//		int result2 = (int)(Math.round(dec2));
+//		int result3 = (int)(Math.round(dec3));
+//		System.out.println(result);
+//		System.out.println(result2);
+//		System.out.println(result3);
+		
+		//RINT
+//		double dec = 5.5;
+//		double dec2 = 6.5;
+//		int result = (int)(Math.rint(dec));
+//		int result2 = (int)(Math.rint(dec2));
+//		System.out.println(result);
+//		System.out.println(result2);
+		
+		//RANDOM
+		// Print angka acak di antara 1 - 10
+//		int number = 1 + (int)(Math.random() * (10-1+1));
+//		System.out.println(number);
+		// Print angka acak di antara 50 - 75
+//		int number = 50 + (int)(Math.random() * (75-50+1));
+//		System.out.println(number);
+		
+		//TORADIAN
+//		int number = 30;
+//		double rad = Math.toRadians(number);
+//		System.out.println(rad);
+		
+		//SIN - COS - TAN
+//		int number = 30;
+//		double sin = Math.sin(Math.toRadians(number));
+//		System.out.println(sin);
+//		double cos = Math.cos(Math.toRadians(number));
+//		System.out.println(cos);
+//		double tan = Math.tan(Math.toRadians(number));
+//		System.out.println(tan);
+	}
+
+}
