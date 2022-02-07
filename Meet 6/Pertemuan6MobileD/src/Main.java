658afd83b133f1468e9bc9bea234042746a5a9f4
youhane <jason.sebastian154@gmail.com>
2022-02-07 21:00:52 +0700

    added files

--- Meet 6/Pertemuan6MobileD/src/Main.java
+++ Meet 6/Pertemuan6MobileD/src/Main.java
@@ -6,23 +6,23 @@ public class Main {
 		// TODO Auto-generated method stub
 		Car innova = new Car();
 		Bike supra = new Bike();
-		
+
 		innova.setName("Innova");
 		innova.setType("Minivan");
 		innova.setWheels(4);
 		innova.setSpeed(150);
-		
+
 		supra.setName("Supra X");
 		supra.setType("Underbone");
 		supra.setWheels(2);
 		supra.setSpeed(120);
-		
+
 		innova.start();
 		innova.brake(50);
-		
+
 		supra.start();
 		supra.service(8);
-		
+
 		System.out.println("Saya punya mobil " + innova.getName());
 	}
 
